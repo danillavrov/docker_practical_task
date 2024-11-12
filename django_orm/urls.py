d2d8e536169c65df850023f351a15c7fc4c84136
Danil <papapapiroska@gmail.com>
2024-11-13 00:44:46 +0300

    orm views fix

--- django_orm/urls.py
+++ django_orm/urls.py
@@ -21,33 +21,36 @@ from django_lib_api.views import *
 from rest_framework import routers
 
 router = routers.SimpleRouter()
-router.register(r'Book',BookViewSet)
-
-router1 = routers.SimpleRouter()
-router1.register(r'Categories', CategoriesViewSet)
-
-router2 = routers.SimpleRouter()
-router2.register(r'BookOwner', BookOwnerViewSet)
-
-router3 = routers.SimpleRouter()
-router3.register(r'BookAuthor', BookAuthorViewSet)
-
-router4 = routers.SimpleRouter()
-router4.register(r'User', UserViewSet)
-
-router5 = routers.SimpleRouter()
-router5.register(r'Author', AuthorViewSet)
+# router.register(r'Book',BookViewSet)
+#
+# router1 = routers.SimpleRouter()
+# router1.register(r'Categories', CategoriesViewSet)
+#
+# router2 = routers.SimpleRouter()
+# router2.register(r'BookOwner', BookOwnerViewSet)
+#
+# router3 = routers.SimpleRouter()
+# router3.register(r'BookAuthor', BookAuthorViewSet)
+#
+# router4 = routers.SimpleRouter()
+# router4.register(r'User', UserViewSet)
+#
+# router5 = routers.SimpleRouter()
+# router5.register(r'Author', AuthorViewSet)
 
 
 
 urlpatterns = [
-    path('api/v1/', include(router.urls)),
-    path('api/v1/', include(router1.urls)),
-    path('api/v1/', include(router2.urls)),
-    path('api/v1/', include(router3.urls)),
-    path('api/v1/', include(router4.urls)),
-    path('api/v1/', include(router5.urls)),
+    # path('api/v1/', include(router.urls)),
+    # path('api/v1/', include(router1.urls)),
+    # path('api/v1/', include(router2.urls)),
+    # path('api/v1/', include(router3.urls)),
+    # path('api/v1/', include(router4.urls)),
+    # path('api/v1/', include(router5.urls)),
     path('admin/', admin.site.urls),
-    path('api/v1/lib', libapiview.as_view()),
+    path('api/v1/Take', TakeApiView.as_view()),
+    path('api/v1/TakeBack', TakeBackApiView.as_view()),
+    path('api/v1/AddBook', AddBookApiView.as_view()),
+    path('api/v1/CreateUser', CreateUserApiView.as_view()),
 
 ]
--- django_orm/views.py
+++ django_orm/views.py
@@ -29,35 +29,86 @@ class BookOwnerViewSet(viewsets.ModelViewSet):
     queryset = BookOwner.objects.all()
     serializer_class = BookOwnerSerializer
 
-class libapiview(APIView):
+# class libapiview(APIView):
+#     def get(self, request):
+#         a = Book.objects.all()
+#         return Response({'books': BookSerializer(a, many=True).data})
+#
+#     def post(self, request):
+#         a = Book.objects.all()
+#
+#         book = request.data.get('book_id')
+#         act = request.data.get('act')
+#         user = request.data.get('user_id')
+#
+#         if act == 'take':
+#             user = User.objects.get(id=user)
+#             book = Book.objects.get(id=book)
+#             BookOwner.objects.create(user_id=user, book_id=book)
+#
+#         if act == 'back':
+#             book = BookOwner.objects.get(id=book)
+#             book.delete()
+#
+#         if act == 'loses':
+#             book1 = BookOwner.objects.get(id=book)
+#             book1.delete()
+#             book = Book.objects.get(id=book)
+#             book.delete()
+#         return Response({'books': BookSerializer(a, many=True).data})
+
+class TakeApiView(APIView):
+    def get(self, request):
+        a = Book.objects.all()
+        b = User.objects.all()
+        return Response({'books': BookSerializer(a, many=True).data, "users": UserSerializer(b, many=True).data})
+    def post(self, request):
+        user_id = request.data.get('user_id')
+        book_id = request.data.get('book_id')
+        print(book_id)
+        print(user_id)
+        user = User.objects.get(id=user_id)
+        book = Book.objects.get(id=book_id)
+        BookOwner.objects.create(user_id=user, book_id=book)
+        return Response("success")
+
+class TakeBackApiView(APIView):
+    def get(self, request):
+        a = BookOwner.objects.all()
+        return Response(BookOwnerSerializer(a, many=True).data)
+    def post(self, request):
+        user_id = request.data.get('user_id')
+        book_id = request.data.get('book_id')
+        print(book_id)
+        print(user_id)
+
+        book = BookOwner.objects.get(book_id=book_id, user_id=user_id)
+        book.delete()
+        return Response("success")
+
+class AddBookApiView(APIView):
     def get(self, request):
         a = Book.objects.all()
         return Response({'books': BookSerializer(a, many=True).data})
-
     def post(self, request):
-        a = Book.objects.all()
+        author = request.data.get('author').split()
+        print(author)
+        category = request.data.get('category')
+        Name = request.data.get('name')
+        Author_id = Author.objects.get(name=author[0])
+        Category_id = Category.objects.get(category=category)
+        Book.objects.create(author_id=Author_id, name=Name, category_id=Category_id)
+        return Response("success")
 
-        book = request.data.get('book_id')
-        act = request.data.get('act')
-        user = request.data.get('user_id')
-
-        if act == 'take':
-            user = User.objects.get(id=user)
-            book = Book.objects.get(id=book)
-            BookOwner.objects.create(user_id=user, book_id=book)
-
-        if act == 'back':
-            book = BookOwner.objects.get(id=book)
-            book.delete()
-
-        if act == 'loses':
-            book1 = BookOwner.objects.get(id=book)
-            book1.delete()
-            book = Book.objects.get(id=book)
-            book.delete()
-        return Response({'books': BookSerializer(a, many=True).data})
-
-
+class CreateUserApiView(APIView):
+    def get(self, request):
+        a = User.objects.all()
+        return Response(UserSerializer(a, many=True).data)
+    def post(self, request):
+        name = request.data.get('name')
+        email = request.data.get('email')
+        User.objects.create(name=name, email=email)
+        return Response("success")
 
 
 
