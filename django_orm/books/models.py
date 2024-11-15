a694420ad33116003fba42b74693ac2d82aea77e
Danil <papapapiroska@gmail.com>
2024-11-15 11:46:34 +0300

    serializers replace

---
+++ django_orm/books/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=50)
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    second_name = models.CharField(max_length=50)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=50)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+class BookAuthor(models.Model):
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+
+
+class BookOwner(models.Model):
+    book_id = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
